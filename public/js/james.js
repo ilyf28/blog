82968b6c46fcb00bb9ef98d40cfc95c65d39e7f9
suhyun Lee <ilyf28@gmail.com>
2016-01-06 13:14:52 +0900

    Add a read-only form

--- public/js/james.js
+++ public/js/james.js
@@ -49,8 +49,16 @@
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(158);
 
+	/*
+	 * Data
+	 */
 	var contacts = [{ key: 1, name: "James K Nelson", email: "james@jamesknelson.com", description: "Front-end Unicorn" }, { key: 2, name: "Jim", email: "jim@example.com" }, { key: 3, name: "Joe" }];
 
+	var newContact = { name: "", email: "", description: "" };
+
+	/*
+	 * Components
+	 */
 	var ContactItem = React.createClass({
 	  displayName: 'ContactItem',
 
@@ -69,13 +77,39 @@
 	  }
 	});
 
+	var ContactForm = React.createClass({
+	  displayName: 'ContactForm',
+
+	  propTypes: {
+	    contact: React.PropTypes.object.isRequired
+	  },
+
+	  render: function render() {
+	    return React.createElement('form', {}, React.createElement('input', {
+	      type: 'text',
+	      placeholder: 'Name (required)',
+	      value: this.props.contact.name
+	    }), React.createElement('input', {
+	      type: 'email',
+	      placeholder: 'Email',
+	      value: this.props.contact.email
+	    }), React.createElement('textarea', {
+	      placeholder: 'Description',
+	      value: this.props.contact.description
+	    }), React.createElement('button', { type: 'submit' }, "Add Contact"));
+	  }
+	});
+
+	/*
+	 * Render
+	 */
 	var contactItemElements = contacts.filter(function (contact) {
 	  return contact.email;
 	}).map(function (contact) {
 	  return React.createElement(ContactItem, contact);
 	});
 
-	var rootElement = React.createElement('div', {}, React.createElement('h1', {}, "Contacts"), React.createElement('ul', {}, contactItemElements));
+	var rootElement = React.createElement('div', {}, React.createElement('h1', {}, "Contacts"), React.createElement('ul', {}, contactItemElements), React.createElement(ContactForm, { contact: newContact }));
 
 	ReactDOM.render(rootElement, document.getElementById('react-app'));
 
--- resources/assets/jsx/james.jsx
+++ resources/assets/jsx/james.jsx
@@ -1,12 +1,22 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+
+/*
+ * Data
+ */
 var contacts = [
   {key: 1, name: "James K Nelson", email: "james@jamesknelson.com", description: "Front-end Unicorn"},
   {key: 2, name: "Jim", email: "jim@example.com"},
   {key: 3, name: "Joe"},
 ]
 
+var newContact = {name: "", email: "", description: ""}
+
+
+/*
+ * Components
+ */
 var ContactItem = React.createClass({
   propTypes: {
     name: React.PropTypes.string.isRequired,
@@ -29,6 +39,38 @@ var ContactItem = React.createClass({
   },
 })
 
+var ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  },
+
+  render: function() {
+    return (
+      React.createElement('form', {},
+        React.createElement('input', {
+          type: 'text',
+          placeholder: 'Name (required)',
+          value: this.props.contact.name,
+        }),
+        React.createElement('input', {
+          type: 'email',
+          placeholder: 'Email',
+          value: this.props.contact.email,
+        }),
+        React.createElement('textarea', {
+          placeholder: 'Description',
+          value: this.props.contact.description,
+        }),
+        React.createElement('button', {type: 'submit'}, "Add Contact")
+      )
+    )
+  },
+})
+
+
+/*
+ * Render
+ */
 var contactItemElements = contacts
   .filter(function(contact) { return contact.email })
   .map(function(contact) { return React.createElement(ContactItem, contact) })
@@ -36,7 +78,8 @@ var contactItemElements = contacts
 var rootElement =
   React.createElement('div', {}, 
     React.createElement('h1', {}, "Contacts"),
-    React.createElement('ul', {}, contactItemElements)
+    React.createElement('ul', {}, contactItemElements),
+    React.createElement(ContactForm, {contact: newContact})
   )
 
 ReactDOM.render(rootElement, document.getElementById('react-app'))

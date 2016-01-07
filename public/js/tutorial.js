7bba3514fd189bcc73b109af78277f961e5018f7
suhyun Lee <ilyf28@gmail.com>
2016-01-07 15:29:42 +0900

    tutorial8

--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -49,6 +49,11 @@ var Comment = React.createClass({
   }
 });
 
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 
 ReactDOM.render(
   <CommentBox />,
